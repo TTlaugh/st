9056661db4642ebf2e098887ca9b10e1f6f95725
Nguyen Le Tien Trien <trien19012004@gmail.com>
2021-12-29 08:47:12 +0700

    change font2

--- config.def.h
+++ config.def.h
@@ -5,13 +5,13 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:pixelsize=15:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=15:antialias=true:autohint=true";
 /* static char *font = "Hack Nerd Font:pixelsize=15:antialias=true:autohint=true"; */
 /* Spare fonts */
 static char *font2[] = {
-	"JoyPixels:pixelsize=15:antialias=true:autohint=true",
-/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+	"JoyPixels:pixelsize=12:antialias=true:autohint=true",
+	"Hack Nerd Font Mono:pixelsize=12:antialias=true:autohint=true", 
+	//"Symbola:pixelsize=12:antialias=true:autohint=true", 
 };
 
 static int borderpx = 2;
